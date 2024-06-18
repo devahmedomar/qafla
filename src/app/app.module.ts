7fa45042384997c13dbc13f0c7877f1f64355a40
Ahmed Omar Abdel Hakim Morsi <159171834+devahmedomar@users.noreply.github.com>
2024-06-18 22:36:37 +0300

    first demo v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './sections/about/about.component';
+import { RoadmapsComponent } from './sections/roadmaps/roadmaps.component';
+import { NavbarComponent } from './sections/navbar/navbar.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    RoadmapsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
